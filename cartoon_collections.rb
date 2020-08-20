3f215693f680d8bf6f5e54f4b6318e0e550a3ee7
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-08-20 04:01:54 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(list)
   # Your code here
+  list.each_with_index do |i,j|
+    puts "#{j+1}. #{i}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(list)
+  list.map! do |name|
+  name.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(list)
+  list.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect do |name|
+    cheese_types.include?(name)
+  end
 end
